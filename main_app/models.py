dd6a438e7751ff6c3299d0bc4c704d47b2a59c2e
haaben-exe <htkidanu@gmail.com>
2025-04-03 16:25:29 -0400

    crud functionality added for toys

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+    ('S', 'Snacks')
+)
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
 
 # Create your models here.
 class Cat(models.Model):
@@ -8,6 +24,7 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return f'({self.id}) {self.name}, is a {self.age} year old {self.breed}. {self.description}'
@@ -16,3 +33,23 @@ class Cat(models.Model):
     def get_absolute_url(self):
         # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
         return reverse('cat-detail', kwargs={'cat_id': self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    ) 
+    
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} for {self.cat.name} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']  # This line makes the newest feedings appear first
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
-from .models import Cat
+from .models import Cat, Toy
+from .forms import FeedingForm
 
 from django.http import HttpResponse
 # Create your views here.
@@ -22,11 +24,21 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # toys = Toy.objects.all()
+
+    # Only get the toys the cat does not have
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
+
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have
+    })
 
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__' # References the Cat model's attributes as fields
+    fields = ['name', 'breed', 'description', 'age'] 
+    # fields = '__all__' References the Cat model's attributes as fields
     # success_url = '/cats/'
 
 class CatUpdate(UpdateView):
@@ -37,3 +49,42 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyList(ListView):
+    model = Toy
+
+class ToyDetail(DetailView):
+    model = Toy
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def associate_toy(request, cat_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole object
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
